c119cbd95af3744cc06a976b9036935c2fd3dea9
Sourcery AI <>
2023-06-23 09:55:01 +0000

    'Refactored by Sourcery'

--- file_utils.py
+++ file_utils.py
@@ -17,11 +17,11 @@ def list_files(in_path):
         for file in filenames:
             filename, ext = os.path.splitext(file)
             ext = str.lower(ext)
-            if ext == '.jpg' or ext == '.jpeg' or ext == '.gif' or ext == '.png' or ext == '.pgm':
+            if ext in ['.jpg', '.jpeg', '.gif', '.png', '.pgm']:
                 img_files.append(os.path.join(dirpath, file))
             elif ext == '.bmp':
                 mask_files.append(os.path.join(dirpath, file))
-            elif ext == '.xml' or ext == '.gt' or ext == '.txt':
+            elif ext in ['.xml', '.gt', '.txt']:
                 gt_files.append(os.path.join(dirpath, file))
             elif ext == '.zip':
                 continue
@@ -31,7 +31,7 @@ def list_files(in_path):
     return img_files, mask_files, gt_files
 
 def saveResult(img_file, img, boxes, dirname='./result/', verticals=None, texts=None):
-        """ save text detection result one by one
+    """ save text detection result one by one
         Args:
             img_file (str): image file name
             img (array): raw image context
@@ -40,37 +40,53 @@ def saveResult(img_file, img, boxes, dirname='./result/', verticals=None, texts=
         Return:
             None
         """
-        img = np.array(img)
+    img = np.array(img)
 
-        # make result file list
-        filename, file_ext = os.path.splitext(os.path.basename(img_file))
+    # make result file list
+    filename, file_ext = os.path.splitext(os.path.basename(img_file))
 
         # result directory
-        res_file = dirname + "res_" + filename + '.txt'
-        res_img_file = dirname + "res_" + filename + '.jpg'
+    res_file = f"{dirname}res_{filename}.txt"
+    res_img_file = f"{dirname}res_{filename}.jpg"
 
-        if not os.path.isdir(dirname):
-            os.mkdir(dirname)
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
 
-        with open(res_file, 'w') as f:
-            for i, box in enumerate(boxes):
-                poly = np.array(box).astype(np.int32).reshape((-1))
-                strResult = ','.join([str(p) for p in poly]) + '\r\n'
-                f.write(strResult)
+    with open(res_file, 'w') as f:
+        for i, box in enumerate(boxes):
+            poly = np.array(box).astype(np.int32).reshape((-1))
+            strResult = ','.join([str(p) for p in poly]) + '\r\n'
+            f.write(strResult)
 
-                poly = poly.reshape(-1, 2)
-                cv2.polylines(img, [poly.reshape((-1, 1, 2))], True, color=(0, 0, 255), thickness=2)
-                ptColor = (0, 255, 255)
-                if verticals is not None:
-                    if verticals[i]:
-                        ptColor = (255, 0, 0)
+            poly = poly.reshape(-1, 2)
+            cv2.polylines(img, [poly.reshape((-1, 1, 2))], True, color=(0, 0, 255), thickness=2)
+            ptColor = (0, 255, 255)
+            if verticals is not None:
+                if verticals[i]:
+                    ptColor = (255, 0, 0)
 
-                if texts is not None:
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    font_scale = 0.5
-                    cv2.putText(img, "{}".format(texts[i]), (poly[0][0]+1, poly[0][1]+1), font, font_scale, (0, 0, 0), thickness=1)
-                    cv2.putText(img, "{}".format(texts[i]), tuple(poly[0]), font, font_scale, (0, 255, 255), thickness=1)
+            if texts is not None:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_scale = 0.5
+                cv2.putText(
+                    img,
+                    f"{texts[i]}",
+                    (poly[0][0] + 1, poly[0][1] + 1),
+                    font,
+                    font_scale,
+                    (0, 0, 0),
+                    thickness=1,
+                )
+                cv2.putText(
+                    img,
+                    f"{texts[i]}",
+                    tuple(poly[0]),
+                    font,
+                    font_scale,
+                    (0, 255, 255),
+                    thickness=1,
+                )
 
-        # Save result image
-        cv2.imwrite(res_img_file, img)
+    # Save result image
+    cv2.imwrite(res_img_file, img)
 
